103582464a552ef6e64e8d3a730f08bc32cda155
ahamedsahib <adsahib39@gmail.com>
2021-06-28 13:28:50 +0530

    Attendance check using Random

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,6 +7,19 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation Program");
+            int present = 1;
+            Random random = new Random();
+            int emp_Input = random.Next(0, 2);
+           
+            if (emp_Input==present)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
         }
     }
 }
