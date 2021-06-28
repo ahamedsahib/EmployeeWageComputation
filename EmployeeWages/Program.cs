7a039d234df10aa245f8c2d7492ba17e6e0aae31
ahamedsahib <adsahib39@gmail.com>
2021-06-28 16:22:29 +0530

    Calculating Wages for month

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,34 +7,45 @@ namespace EmployeeWages
         public const int fullTime = 1;
         public const int partTime = 2;
         public const int wage_per_hour = 20;
+        public const int working_day = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation Program");
             
             int working_hour_per_day = 0;
-            int wage_per_day = 0;
+            int total_wage = 0;
+            int fullday = 0;
+            int halfday = 0;
+            int absent = 0;
 
-            Random random = new Random();
-            int emp_Input = random.Next(0, 3);
-
-            switch (emp_Input)
+            for (int day = 1; day <= 20; day++)
             {
-                case fullTime:
-                    Console.WriteLine("Employee is present and working as full time");
-                    working_hour_per_day = 8;
-                    break;
-                case partTime:
-                    Console.WriteLine("Employee is present and working as part time");
-                    working_hour_per_day = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    break;
 
+                Random random = new Random();
+                int emp_Input = random.Next(0, 3);
+                
+                switch (emp_Input)
+                {
+                    case fullTime:
+                        working_hour_per_day = 8;
+                        fullday++;
+                        break;
+                    case partTime:
+                        working_hour_per_day = 4;
+                        halfday++;
+                        break;
+                    default:
+                        working_hour_per_day = 0;
+                        absent++;
+                        break;
+
+                }
+
+                total_wage+= working_hour_per_day * wage_per_hour;
+ 
             }
-           
-            wage_per_day = working_hour_per_day * wage_per_hour;
-            Console.WriteLine($"The wage of Employee per day: {wage_per_day} ");
+            Console.WriteLine($"Employee woked in a month as\nFullTime: {fullday} \nParTime: {halfday}\nAbsent: {absent}");
+            Console.WriteLine($"The Wages of Employee per month: {total_wage} ");
 
         }
     }
