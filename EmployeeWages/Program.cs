8cf8beac56417be1936ae9688bb07a0d73c560d7
ahamedsahib <adsahib39@gmail.com>
2021-06-28 15:39:39 +0530

    Calculating Part time Employee Wage per day

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,18 +7,24 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation Program");
-            int present = 1;
+            int fullTime = 1;
+            int partTime = 2;
             int wage_per_hour = 20;
             int working_hour_per_day = 0;
             int wage_per_day = 0;
 
             Random random = new Random();
-            int emp_Input = random.Next(0, 2);
+            int emp_Input = random.Next(0, 3);
            
-            if (emp_Input==present)
+            if (emp_Input==fullTime)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is present and working as full time");
                 working_hour_per_day = 8;
+            }
+            else if (emp_Input == partTime)
+            {
+                Console.WriteLine("Employee is present and working as part time");
+                working_hour_per_day = 4;
             }
             else
             {
