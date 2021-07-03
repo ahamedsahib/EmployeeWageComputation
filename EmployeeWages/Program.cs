238e72107fc737ef4ea44bdb799c2af314d05395
ahamedsahib <adsahib39@gmail.com>
2021-07-03 23:13:04 +0530

    [Ahamed]compute

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,29 +6,26 @@ namespace EmployeeWages
     {
         public const int fullTime = 1;
         public const int partTime = 2;
-        public const int wage_per_hour = 20;
-        public const int max_working_day = 20;
-        public const int max_working_hour = 100;
-
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation Program");
-            ComputeWage();
+            ComputeWage("Amazon",30,19,85);
+            ComputeWage("Google", 40,22,90);
         }
-        public static void ComputeWage()
+        public static void ComputeWage(string company,int wage_per_hour,int max_working_day,int  max_working_hour)
         {
             //initalizing instance variables
-            int working_hour = 0;
+            int working_hour;
             int total_hour = 0;
             int working_day = 0;
 
 
-            while (working_hour <= max_working_hour && working_day <= max_working_day)
+            while (total_hour <= max_working_hour && working_day < max_working_day)
             {
 
                 Random random = new Random();
                 int emp_Input = random.Next(0, 3);
-
                 switch (emp_Input)
                 {
                     case fullTime:
@@ -49,7 +46,7 @@ namespace EmployeeWages
             //Compute total wage
             int total_wage = total_hour * wage_per_hour;
             Console.WriteLine($"Working day of Employee {working_day - 1}\nTotal Working hours {total_hour}");
-            Console.WriteLine($"The Wages of Employee per month: {total_wage} ");
+            Console.WriteLine($"The Employee Wage of {company} per Month is {total_wage}");
 
         }
     }
