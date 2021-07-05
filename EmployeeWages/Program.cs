b36fcc8e51908d7cc8512742dad4c408e79b0c7b
ahamedsahib <adsahib39@gmail.com>
2021-07-05 17:48:02 +0530

    [Ahamed]To manage employee wage of multiple Company

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -8,11 +8,11 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation Program");
-            EmpBuilderArray amazon = new EmpBuilderArray("Amazon", 30, 19, 85);
-            amazon.ComputeWage();
-            EmpBuilderArray google = new EmpBuilderArray("Google", 40, 22, 90);
-            google.ComputeWage();
-            Console.WriteLine(google.toString());
+            EmpBuilderArray emp = new EmpBuilderArray();
+            emp.AddCompanyWage("Amazon", 30, 19, 85);
+            emp.AddCompanyWage("Google", 40, 22, 90);
+            emp.ComputeEmpWage();
+
         }
         
     }
